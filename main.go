e88d42fc640d2dae07119855a4b11931bc18849a
Taweechai Yuenyang <krumii.it@gmail.com>
2023-07-03 13:36:54 +0700

    update db

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/abe27/vcst/api.v1/configs"
@@ -12,9 +13,10 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+	"gocloud.dev/postgres" // Sqlite driver based on CGO
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func init() {
@@ -23,28 +25,28 @@ func init() {
 		panic(err)
 	}
 
-	// // initial database
-	// dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s sslmode=%s TimeZone=%s", os.Getenv("DBHOST"), os.Getenv("DBUSER"), os.Getenv("DBNAME"), os.Getenv("DBPORT"), os.Getenv("DBPASSWORD"), os.Getenv("SSLMODE"), os.Getenv("TZNAME"))
-	// configs.Store, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-	// 	DisableAutomaticPing:                     true,
-	// 	DisableForeignKeyConstraintWhenMigrating: false,
-	// 	SkipDefaultTransaction:                   true,
-	// 	NowFunc: func() time.Time {
-	// 		return time.Now().Local()
-	// 	},
-	// 	NamingStrategy: schema.NamingStrategy{
-	// 		TablePrefix:   "tbt_", // table name prefix, table for `User` would be `t_users`
-	// 		SingularTable: false,  // use singular table name, table for `User` would be `user` with this option enabled
-	// 		NoLowerCase:   false,  // skip the snake_casing of names
-	// 		NameReplacer:  strings.NewReplacer("CID", "Cid"),
-	// 	},
-	// })
+	// initial database
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s sslmode=%s TimeZone=%s", os.Getenv("DBHOST"), os.Getenv("DBUSER"), os.Getenv("DBNAME"), os.Getenv("DBPORT"), os.Getenv("DBPASSWORD"), os.Getenv("SSLMODE"), os.Getenv("TZNAME"))
+	configs.Store, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		DisableAutomaticPing:                     true,
+		DisableForeignKeyConstraintWhenMigrating: false,
+		SkipDefaultTransaction:                   true,
+		NowFunc: func() time.Time {
+			return time.Now().Local()
+		},
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "tbt_", // table name prefix, table for `User` would be `t_users`
+			SingularTable: false,  // use singular table name, table for `User` would be `user` with this option enabled
+			NoLowerCase:   false,  // skip the snake_casing of names
+			NameReplacer:  strings.NewReplacer("CID", "Cid"),
+		},
+	})
 
-	// github.com/mattn/go-sqlite3
-	configs.Store, err = gorm.Open(sqlite.Open("database/gorm.db"), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to database")
-	}
+	// // github.com/mattn/go-sqlite3
+	// configs.Store, err = gorm.Open(sqlite.Open("database/gorm.db"), &gorm.Config{})
+	// if err != nil {
+	// 	panic("Failed to connect to database")
+	// }
 
 	dsnFormula := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s&encrypt=disable&connection+timeout=30", os.Getenv("DB_FORMULA_MSSQL_USER"), os.Getenv("DB_FORMULA_MSSQL_PASSWORD"), os.Getenv("DB_FORMULA_MSSQL_HOST"), os.Getenv("DB_FORMULA_MSSQL_PORT"), os.Getenv("DB_FORMULA_MSSQL_DATABASE"))
 	// dsnFormula := "sqlserver://sa:ADSads123@localhost:1433?database=FormulaDB"
